ed7958843fe239a56d1684b0c41d1be71d473382
nicole926 <nicolemendes.dev@gmail.com>
2026-04-28 14:54:56 -0300

    commit

---
+++ funções/setTimeout&setInterval/script.js
@@ -0,0 +1,33 @@
+// setTimeout
+// primeiro parâmetro é uma function
+// segundo parâmetro é o intervalo em milisegundos (1s é 1000 ms)
+// esse tempo de espera se chama: chamada assíncrona.
+
+console.log("antes do setTimeout");
+
+setTimeout(function(){
+ console.log("Testando o setTimeout");
+}, 2000)
+
+console.log("Depois do setTimeout");
+
+// setInterval
+
+setInterval(function(){
+    console.log("Testando o setInterval");
+}, 1000);
+
+// clearTimeout
+
+var x = 0;
+
+var myTimer = setTimeout(function(){
+    console.log("O x é 0");
+}, 1500);
+
+x = 5;
+
+if(x > 0){
+    clearTimeout(myTimer);
+    console.log("O x passou de 0");
+}
