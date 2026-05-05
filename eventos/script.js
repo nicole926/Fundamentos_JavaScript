39b52f5b4cd553284931e90a870f61ade924f489
nicole926 <nicolemendes.dev@gmail.com>
2026-05-05 14:56:40 -0300

    commit

--- eventos/script.js
+++ eventos/script.js
@@ -62,4 +62,16 @@ subtitle.addEventListener("mouseout", function(){
 // keydown
 document.addEventListener("keydown", function(event){
     console.log(event.key);
+
+    if(event.key === "Enter"){
+        console.log("Apertou Enter");
+    };
+});
+
+// keyup
+
+document.addEventListener("keyup", function(e){
+    if(event.key === "Enter"){
+        console.log("Soltou o Enter");
+    };
 });
---
+++ json/script.js
@@ -0,0 +1,39 @@
+const objs = [
+    {
+    nome: "Maria",
+    idade: 30,
+    esta_trabalhando: true,
+    detalhes_profissao: {
+        profissao: "Programador",
+        empresa: "Empresa x"
+    },
+    hobbies :["Programar", "correr", "ler"]
+    },
+    {
+        nome: "João",
+        idade: 25,
+        esta_trabalhando: false,
+        detalhes_profissao: {
+            profissao: null,
+            empresa: null
+        },
+        hobbies :["jogar", "academia"]
+    }
+] 
+
+console.log(objs);
+// JSON
+// convertendo objeto para JSON
+const jsonData = JSON.stringify(objs);
+console.log(jsonData);
+console.log(typeof jsonData);
+
+// converter json para objeto
+const objData = JSON.parse(jsonData);
+
+console.log(objData);
+console.log(typeof objData);
+
+objData.map((pessoa) => {
+    console.log(pessoa.nome);
+})
